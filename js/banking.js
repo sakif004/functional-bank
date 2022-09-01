68e21a1d6a9ff154cc597758b887e0dedb0b5b34
unknown <sakifur35-2749@diu.edu.bd>
2022-09-01 18:00:19 +0600

    Nested function

--- js/banking.js
+++ js/banking.js
@@ -16,10 +16,20 @@ function upadateTotalField(totalFieldId, depositAmount) {
     totalElement.innerText = previousTotal + depositAmount;
 };
 
-function updateBalance(amount, isAdd) {
+function getCurrentBalance() {
     const balanceTotal = document.getElementById('balance-total');
     const balanceTotalText = balanceTotal.innerText;
     const previousBalanceTotal = parseFloat(balanceTotalText);
+    return previousBalanceTotal;
+};
+
+function updateBalance(amount, isAdd) {
+    const balanceTotal = document.getElementById('balance-total');
+    /*
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+     */
+    const previousBalanceTotal = getCurrentBalance();
     if (isAdd == true) {
         balanceTotal.innerText = previousBalanceTotal + amount;
     }
@@ -35,7 +45,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const depositAmountText = depositInput.value;
     const depositAmount = parseFloat(depositAmountText); 
     */
-    const depositAmount = getInputValue('deposit-input');
+
 
     // get and update deposit total 
     /* 
@@ -44,7 +54,7 @@ document.getElementById('deposit-button').addEventListener('click', function () 
     const previousDepositTotal = parseFloat(depositTotalText);
     depositTotal.innerText = previousDepositTotal + depositAmount;
      */
-    upadateTotalField('deposit-total', depositAmount);
+
 
     // update balance 
     /* 
@@ -54,7 +64,11 @@ document.getElementById('deposit-button').addEventListener('click', function () 
 
     balanceTotal.innerText = previousBalanceTotal + depositAmount;
  */
-    updateBalance(depositAmount, true);
+    const depositAmount = getInputValue('deposit-input');
+    if (depositAmount > 0) {
+        upadateTotalField('deposit-total', depositAmount);
+        updateBalance(depositAmount, true);
+    }
 });
 
 // handle withdraw button 
@@ -64,7 +78,7 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const withdrawAmountText = withdrawInput.value;
     const withdrawAmount = parseFloat(withdrawAmountText);
     */
-    const withdrawAmount = getInputValue('withdraw-input');
+
 
     // get and update withdraw total 
     /* 
@@ -75,7 +89,7 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     withdrawTotal.innerText = previouswithdrawTotal + withdrawAmount;
  */
 
-    upadateTotalField('withdraw-total', withdrawAmount);
+
 
     // update balance after withdraw 
     /* 
@@ -84,7 +98,12 @@ document.getElementById('withdraw-button').addEventListener('click', function ()
     const previousBalanceTotal = parseFloat(balanceTotalText);
     balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
  */
-    updateBalance(withdrawAmount, false);
+    const withdrawAmount = getInputValue('withdraw-input');
+    const currentBalance = getCurrentBalance();
+    if (withdrawAmount > 0 && withdrawAmount < currentBalance) {
+        upadateTotalField('withdraw-total', withdrawAmount);
+        updateBalance(withdrawAmount, false);
+    }
     // clear withdraw input field 
     // withdrawInput.value = '';
 
